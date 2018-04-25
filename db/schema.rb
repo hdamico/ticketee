74590850e5e8d2e187251495131b55ea5ffb1ada
Hernan <hernan@oyga.me>
2018-04-25 10:29:09 -0300

    Add title functionallity for project show page

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180424173638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "description"
